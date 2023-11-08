2fec046c1725482345028d1d740181d43ab7997e
iplmr <45397969+iplmr@users.noreply.github.com>
2023-11-08 09:20:21 +0300

    hw

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const blackButton = document.getElementById("black-button");
 const goldButton = document.getElementById("gold-button");
 const pinkButton = document.getElementById("pink-button");
@@ -11,16 +10,22 @@ const pinkWatch = document.getElementById("pink");
 const blueWatch = document.getElementById("blue");
 const redWatch = document.getElementById("red");
 
-
 function showWatch(watchToShow) {
   const watches = [blackWatch, goldWatch, pinkWatch, blueWatch, redWatch];
   watches.forEach(watch => {
     watch.style.display = "none";
   });
 
-  
+  const currentTime = new Date();
+  const hours = currentTime.getHours().toString().padStart(2, "0");
+  const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+  const seconds = currentTime.getSeconds().toString().padStart(2, "0");
+
+  const timeDisplay = watchToShow.querySelector(".time-display");
+  if (timeDisplay) {
+    timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
+  }
   watchToShow.style.display = "block";
-  
 }
 
 blackButton.addEventListener("click", function () {
